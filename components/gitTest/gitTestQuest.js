71a0065ab8df446ab04662a5b2c1351f356db7a3
Yehor Voronov <380954214108tt@gmail.com>
2022-05-29 15:14:07 +0200

    add new in git test22222222222222222

---
+++ components/gitTest/gitTestQuest.js
@@ -0,0 +1,17 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet,Pressable, Text, View } from 'react-native';
+
+
+export default function GitTest() {
+
+    return (
+        <View style={styles.container}>
+          gitTestedwtfgesrg
+          gitTestedwtfgesrg
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+
+});
