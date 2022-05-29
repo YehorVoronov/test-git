022e57324a731897b2a4249e2dcf91df3caf5c63
Yehor Voronov <380954214108tt@gmail.com>
2022-05-29 14:46:11 +0200

    add git test

---
+++ components/gitTest/gitTest.js
@@ -0,0 +1,16 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet,Pressable, Text, View } from 'react-native';
+
+
+export default function GitTest() {
+
+    return (
+        <View style={styles.container}>
+          gitTest
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+
+});
